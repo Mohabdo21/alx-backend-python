19cb01fc96474f0dcd0e19adb8d7f54b51a53570
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-06-11 16:07:19 +0400

    i know it wrong

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,10 +5,10 @@ This module contains an asynchronous generator.
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     An asynchronous generator that yields random numbers.
 
