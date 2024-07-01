f665413cbae0c16277b73bde11dab2a161d9cabf
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-07-01 10:28:33 +0400

    6. More patching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -45,3 +45,52 @@ class TestGithubOrgClient(TestCase):
             self.assertEqual(GithubOrgClient(
                 "google")._public_repos_url,
                 "https://api.github.com/orgs/google/repos",)
+
+    @patch("client.get_json")
+    def test_public_repos(self, mocked_get_json: MagicMock):
+        """Tests the public_repos method."""
+        test_payload = {
+            "repos_url": "https://api.github.com/orgs/google/repos",
+            "repos": [
+                {
+                    "id": 7697149,
+                    "node_id": "MDEwOlJlcG9zaXRvcnk3Njk3MTQ5",
+                    "name": "episodes.dart",
+                    "full_name": "google/episodes.dart",
+                    "private": False,
+                    "forks": 22,
+                    "open_issues": 0,
+                    "watchers": 12,
+                    "default_branch": "master",
+                    "permissions": {
+                        "admin": False,
+                        "push": False,
+                        "pull": True
+                    }
+                },
+                {
+                    "id": 7776515,
+                    "node_id": "MDEwOlJlcG9zaXRvcnk3Nzc2NTE1",
+                    "name": "cpp-netlib",
+                    "full_name": "google/cpp-netlib",
+                    "private": False,
+                    "forks": 59,
+                    "open_issues": 0,
+                    "watchers": 292,
+                    "default_branch": "master",
+                    "permissions": {
+                        "admin": False,
+                        "push": False,
+                        "pull": True
+                    }
+                },
+            ]
+        }
+        mocked_get_json.return_value = test_payload["repos"]
+        with patch("client.GithubOrgClient._public_repos_url",
+                   new_callable=PropertyMock) as mocked_public_repos_url:
+            mocked_public_repos_url.return_value = test_payload["repos_url"]
+            self.assertEqual(GithubOrgClient("google").public_repos(), [
+                             "episodes.dart", "cpp-netlib"])
+            mocked_public_repos_url.assert_called_once()
+        mocked_get_json.assert_called_once()
