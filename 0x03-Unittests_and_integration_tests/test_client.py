8cb601178bb433b2a52697b372be48571018a56a
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-07-01 13:33:29 +0400

    Single instance of GithubOrgClient

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -151,17 +151,18 @@ class TestIntegrationGithubOrgClient(TestCase):
         cls.get_patcher = patch("requests.get", side_effect=get_payload)
         cls.get_patcher.start()
 
+        # Single instance of GithubOrgClient
+        cls.client = GithubOrgClient("google")
+
     def test_public_repos(self) -> None:
         """Tests the public_repos method."""
-        self.assertEqual(GithubOrgClient(
-            "google").public_repos(), self.expected_repos)
+        self.assertEqual(self.client.public_repos(), self.expected_repos)
 
     def test_public_repos_with_license(self) -> None:
         """Tests the public_repos method with a license."""
-        self.assertEqual(
-            GithubOrgClient("google").public_repos(license="apache-2.0"),
-            self.apache2_repos,
-        )
+        self.assertEqual(self.client.public_repos(license="apache-2.0"),
+                         self.apache2_repos,
+                         )
 
     @classmethod
     def tearDownClass(cls) -> None:
